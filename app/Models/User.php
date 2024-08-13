77b14740d2868a5a541a46ed94187ca4e6d4f6b9
Hamed EL-Asma <hamedelasma@gmail.com>
2024-08-13 10:45:15 +0200

    configure user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,30 +18,21 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
         'password',
+        'phone',
+        'gender',
+        'DOB',
+        'avatar_url',
+        'server_id'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+//    /**
+//     * The attributes that should be hidden for serialization.
+//     *
+//     * @var array<int, string>
+//     */
+//    protected $hidden = [
+//        'password',
+//    ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
 }
