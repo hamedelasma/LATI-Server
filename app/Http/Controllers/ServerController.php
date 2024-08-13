7b2ab242a397ef69de8a9bf9bb45a15dff2df081
Hamed EL-Asma <hamedelasma@gmail.com>
2024-08-13 14:55:54 +0200

    Day three done

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Server;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ServerController extends Controller
 {
-    //
+
+    public function store(Request $request)
+    {
+        $inputs = $request->validate([
+            'name' => ['required', 'string'],
+        ]);
+        $code = $this->generateCode();
+        auth()->user()->servers()->create([
+            'code' => $code,
+            'name' => $inputs['name']
+        ]);
+        return response()->json([
+            'message' => 'server created successfully',
+            'code' => $code
+        ]);
+
+
+    }
+
+    private function generateCode(): string
+    {
+        $code = Str::random(6);
+        if (Server::where('code', '=', $code)->exists()) {
+            $this->generateCode();
+        }
+        return $code;
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ServerController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,5 +14,6 @@ Route::middleware('auth:api')->group(function () {
 
     Route::post('logout', [UserController::class, 'logout']);
     Route::get('user', [UserController::class, 'user']);
-    Route::post('refresh',[UserController::class,'refresh']);
+    Route::post('refresh', [UserController::class, 'refresh']);
+    Route::post('servers', [ServerController::class, 'store']);
 });
